3c45fc2c80f71ae80345336b916978744ac231f1
aprilmadams <aprilmadams@yahoo.com>
2017-04-01 00:36:02 -0700

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall these functions will save processing power because it will create a special matrix and then create an inverse.  The special matrix saves on computation.
 
-## Write a short comment describing this function
+## This function is going to take set the inverse and then get the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+    inv <- NULL
+    set <- function(y){
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solveMatrix) inv <<- solveMatrix
+    getInverse <- function() inv
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  }
 
 
-## Write a short comment describing this function
 
+## This function will inverse the matrix above
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
 }
